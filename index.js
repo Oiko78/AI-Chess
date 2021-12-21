afe9c366745cc38a7ef95a530ceae55bf6ada2cb
oiko78 <oliver.chico@binus.ac.id>
2021-12-21 21:50:08 +0700

    refactor

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
+const onDragStart = (source, piece, position, orientation) => {
+  console.log(source, piece, position, orientation);
+};
+
 const config = {
   draggable: true,
+  dropOffBoard: "snapback",
   position: "start",
+  onDragStart: onDragStart,
+  // onDragMove: onDragMove,
+  // onDrop: onDrop,
+  // onMouseoutSquare: onMouseoutSquare,
+  // onMouseoverSquare: onMouseoverSquare,
+  // onMoveEnd: onMoveEnd,
+  // onSnapbackEnd: onSnapbackEnd,
+  // onSnapEnd: onSnapEnd,
+  orientation: "white",
+  showNotation: true,
+  sparePieces: false,
+  pieceTheme: "img/chesspieces/{piece}.png",
+  appearSpeed: 200,
+  moveSpeed: 200,
+  snapbackSpeed: 50,
+  snapSpeed: 25,
+  trashSpeed: 100,
 };
 const board = Chessboard("board", config);
 const chess = new Chess();
-
-board.position(chess.fen());
-
-// $(document).click(() => {
-//   const moves = chess.moves({ verbose: true });
-//   const move = moves[0];
-//   chess.move(move);
-//   board.position(chess.fen());
-// });
